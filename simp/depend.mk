82294208fd3c958deaff718e19bb75d4c329bc28
Mate Soos <soos.mate@gmail.com>
2021-03-14 19:41:22 +0100

    Initial commit

---
+++ simp/depend.mk
@@ -0,0 +1,41 @@
+/home/soos/development/sat_solvers/glucose-syrup-4.1/simp/SimpSolver.o /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/SimpSolver.or /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/SimpSolver.od /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/SimpSolver.op: \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/SimpSolver.cc \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Sort.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/SolverStats.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/System.h
+/home/soos/development/sat_solvers/glucose-syrup-4.1/simp/Main.o /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/Main.or /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/Main.od /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/Main.op: /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/Main.cc \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/System.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/IntTypes.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/ParseUtils.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../utils/Options.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Vec.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/XAlloc.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/Dimacs.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/SolverTypes.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Alg.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Map.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Alloc.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../simp/SimpSolver.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Queue.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/Solver.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Heap.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/BoundedQueue.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/Constants.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../mtl/Clone.h \
+ /home/soos/development/sat_solvers/glucose-syrup-4.1/simp/../core/SolverStats.h
